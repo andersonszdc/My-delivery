2875d21dae166e5db586cc0fd9020906d43f50fe
anderson <andersonszdc@gmail.com>
2021-12-29 10:30:16 -0300

    connected apollo-client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,29 +1,37 @@
-import { NextPage } from "next";
-import { AppProps } from "next/app";
-import React, { ComponentType } from "react";
-import { OrderContextProvider } from "../contexts/OrderContext";
-import Global from '../styles/global'
+import { NextPage } from 'next';
+import { AppProps } from 'next/app';
+import React, { ComponentType } from 'react';
+import { OrderContextProvider } from '../contexts/OrderContext';
+import Global from '../styles/global';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 type Page = NextPage & {
-  layout?: ComponentType
-}
+  layout?: ComponentType;
+};
 
 type Props = AppProps & {
-  Component: Page
-}
+  Component: Page;
+};
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: Props) {
-  const CustomLayout = Component.layout ? Component.layout : React.Fragment
+  const CustomLayout = Component.layout ? Component.layout : React.Fragment;
   return (
     <>
-      <OrderContextProvider>
-        <Global />
-        <CustomLayout>
-          <Component {...pageProps} />
-        </CustomLayout>
-      </OrderContextProvider>
+      <ApolloProvider client={client}>
+        <OrderContextProvider>
+          <Global />
+          <CustomLayout>
+            <Component {...pageProps} />
+          </CustomLayout>
+        </OrderContextProvider>
+      </ApolloProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
