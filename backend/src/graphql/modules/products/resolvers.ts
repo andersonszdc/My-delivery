b83d6d04917f167381f0efa59851adc84d34d869
anderson <andersonszdc@gmail.com>
2021-12-29 07:03:56 -0300

    created updateProduct function

--- backend/src/graphql/modules/products/resolvers.ts
+++ backend/src/graphql/modules/products/resolvers.ts
@@ -1,12 +1,17 @@
 import AddProduct from "../../../services/AddProduct";
 import GetAllProducts from "../../../services/GetAllProducts";
+import UpdateProduct from "../../../services/UpdateProduct";
 
 export default {
   Query: {
     products: async () => await GetAllProducts(),
   },
   Mutation: {
-    addProduct: async (_, { name, description }) =>
-      await AddProduct({ name, description }),
+    addProduct: async (_, { name, description }) => {
+      return await AddProduct({ name, description });
+    },
+    updateProduct: async (_, { id, name, description }) => {
+      return await UpdateProduct({ id, name, description });
+    },
   },
 };
