16dda6b36bfbb885479bd573586b1c3c84b5db68
anderson <andersonszdc@gmail.com>
2022-01-05 13:47:22 -0300

    created paymentIntent

--- backend/src/services/CreatePaymentIntent.ts
+++ backend/src/services/CreatePaymentIntent.ts
@@ -1,5 +1,20 @@
-const CreatePaymentIntent = (items: string) => {
-    return items
-}
+import Stripe from "stripe";
 
-export default CreatePaymentIntent
+const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY,
+  { apiVersion: "2020-08-27" }
+);
+
+console.log(process.env.STRIPE_SECRET_KEY)
+console.log(process.env.NODE_ENV)
+
+const CreatePaymentIntent = async (items: string) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "brl",
+    payment_method_types: ["card"],
+  });
+  return paymentIntent.client_secret;
+};
+
+export default CreatePaymentIntent;
